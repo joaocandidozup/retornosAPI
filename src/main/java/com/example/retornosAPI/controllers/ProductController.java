957622b8ad96a5103ff63fcc7cce79548b605744
joaocandidozup <joao.candido@zup.com>
2025-01-23 11:24:10 -0300

    refactor: add standardized response for product creation

--- src/main/java/com/example/retornosAPI/controllers/ProductController.java
+++ src/main/java/com/example/retornosAPI/controllers/ProductController.java
@@ -1,11 +1,12 @@
 package com.example.retornosAPI.controllers;
 
-import com.example.retornosAPI.models.Product;
+import com.example.retornosAPI.dtos.Product;
+import com.example.retornosAPI.responses.ApiResponse;
 import com.example.retornosAPI.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -18,23 +19,30 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        return ResponseEntity.ok(service.createProduct(product));
+    public ResponseEntity<ApiResponse<Product>> createProduct(@Valid @RequestBody Product product) {
+        Product productCreated = service.createProduct(product);
+
+        ApiResponse<Product> response = new ApiResponse<>(
+                "success",
+                "Produto cadastrado com sucesso!",
+                productCreated
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getProductById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(service.getAllProducts());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        service.deleteProduct(id);
-        return ResponseEntity.noContent().build();
-    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+//        return ResponseEntity.ok(service.getProductById(id));
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<Product>> getAllProducts() {
+//        return ResponseEntity.ok(service.getAllProducts());
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+//        service.deleteProduct(id);
+//        return ResponseEntity.noContent().build();
+//    }
 }
