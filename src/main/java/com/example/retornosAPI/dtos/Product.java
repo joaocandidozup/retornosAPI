636bfd294e14815b34ea74f971b2cb99440a5218
joaocandidozup <joao.candido@zup.com>
2025-01-27 09:44:44 -0300

    refactor: add new validations

--- src/main/java/com/example/retornosAPI/dtos/Product.java
+++ src/main/java/com/example/retornosAPI/dtos/Product.java
@@ -1,12 +1,10 @@
 package com.example.retornosAPI.dtos;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 
 public record Product(
         Long id,
+        @NotBlank(message = "O nome é obrigatorio")
         @Pattern(regexp = "[a-zA-Z0-9\\s]{3,100}", message = "O nome deve ter no mínimo 3 e no máximo 100 caracteres")
         String name,
         @Pattern(regexp = "[a-zA-Z0-9\\s]{0,500}", message = "A descrição pode ter no máximo 500 caracteres")
@@ -14,9 +12,11 @@ public record Product(
         @Positive(message = "O preço tem que ser maior que zero")
         @NotNull(message = "O preço é obrigatorio")
         Double price,
+        @NotNull(message = "A quantidade em estoque é obrigatoria")
         @Min(value = 0, message = "A quantidade em estoque deve ser um número inteiro maior ou igual a 0")
-        int quantityStock,
-        @Pattern(regexp = "Eletrônicos|Roupas|Alimentos",message = "Categoria invalida! escolha entre: Eletrônicos, Roupas, Alimentos")
+        Integer quantityStock,
+        @NotBlank(message = "A categoria é obrigatoria")
+        @Pattern(regexp = "Eletrônicos|Roupas|Alimentos", message = "Categoria invalida! escolha entre: Eletrônicos, Roupas, Alimentos")
         String category
 ) {
 
